64a9a125f9a46bc30f7ed8587e820d476ff9d107
Matheus <matheuser1984@gmail.com>
2023-09-16 16:41:31 -0300

    Ola-Mundo Finalizado

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,13 +1,13 @@
-import Post from "componentes/Post";
+import PostCard from "componentes/PostCard";
 import styles from './Inicio.module.css'
 import posts from 'json/posts.json'
 
-export default function Inicio(post) {
+export default function Inicio() {
     return (
        
         <ul className={styles.posts}>
             {posts.map((posts)=>(
-                <li key={posts.id}><Post post={posts}/></li>
+                <li key={posts.id}><PostCard post={posts}/></li>
             ))}
 
         </ul>
--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,24 @@ import Inicio from "./paginas/inicio";
 import Menu from "./componentes/menu";
 import Rodape from "componentes/Rodape";
 import PagPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
 
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
       <Menu/>
       <Routes>
         <Route path="/" element={<PagPadrao/>}>
 
           <Route index element={<Inicio/>}/>
           <Route path="sobremim" element={<SobreMim/>}/>
-
         </Route>
-        
-        <Route path="*" element={<div>Página não encontrada</div>}/>
+        <Route path="posts/:id" element={<Post/>}/>
+        <Route path="*" element={<NaoEncontrada/>}/>
       </Routes>
       <Rodape/>
     </BrowserRouter>
